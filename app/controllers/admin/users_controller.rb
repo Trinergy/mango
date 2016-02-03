5cbb1f99be9a24d4edac386d11ef10921e5f45fa
Trinergy <wu.kenny1221@gmail.com>
2016-02-03 02:08:50 +0000

    add admin checkbox to create user, pagnation

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -9,12 +9,26 @@ class Admin::UsersController < ApplicationController
     @users = User.order(:lastname).page params[:page]
   end
 
+  def new
+    @user = User.new
+  end
+
   def show
     @user = User.find(params[:id])
   end
 
   def edit
     @user = User.find(params[:id])
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      redirect_to admin_users_path
+    else
+      render :new
+    end
   end
 
   def update
@@ -27,11 +41,17 @@ class Admin::UsersController < ApplicationController
     end
   end
 
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    redirect_to admin_users_path
+  end
+
   protected
 
   def user_params
     params.require(:user).permit(
-      :firstname, :lastname, :email
+      :firstname, :lastname, :email, :admin, :password, :password_confirmation
     )
   end
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
   paginates_per 10
 
-  has_many :reviews
+  has_many :reviews, dependent: :destroy
 
   has_secure_password
 
@@ -20,5 +20,15 @@ class User < ActiveRecord::Base
   def full_name
     "#{firstname} #{lastname}"
   end
+
+  # def admin=(value)
+  #   if value == '1'
+  #     true
+  #   elsif value == '0'
+  #     false
+  #   else
+  #     value
+  #   end
+  # end
 
 end
